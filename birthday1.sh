cfc2c18b1d3045d7640c63bfb53ca333ce877406
krishram2k3 <ramkrishna2k3@hotmail.com>
2025-07-04 18:00:39 +0530

    Update birthday1.sh

--- birthday1.sh
+++ birthday1.sh
@@ -2,21 +2,21 @@
 # Change this code version1
 BIRTHDATE="may 17, 1982"
 Presents=10
-BIRTHDAY=`date -d "$BIRTHDATE" +%A`
+date -d "$BIRTHDATE" +%A`
 
 # Testing code - do not change it
 
-if  "$BIRTHDATE" == "may 17, 1982"  ; then
+if [ "$BIRTHDATE" == "may 17, 1982" ] ; then
     echo "BIRTHDATE is correct, it is $BIRTHDATE"
 else
     echo "BIRTHDATE is incorrect - please retry"
 fi
-if  $Presents == 10  ; then
+if [ $Presents == 10 ] ; then
     echo "I have received $Presents presents"
 else
     echo "Presents is incorrect - please retry"
 fi
-if  "$BIRTHDAY" == "Monday"  ; then
+if [ "$BIRTHDAY" == "Monday" ] ; then
     echo "I was born on a $BIRTHDAY"
 else
     echo "BIRTHDAY is incorrect - please re-retry"
