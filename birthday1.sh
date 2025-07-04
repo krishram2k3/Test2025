22fe6e1f30afaae6e672e1cb37db4d364d47f275
krishram2k3 <ramkrishna2k3@hotmail.com>
2025-07-04 16:34:50 +0530

    added file

---
+++ birthday1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Change this code
+BIRTHDATE="may 17, 1982"
+Presents=10
+BIRTHDAY=`date -d "$BIRTHDATE" +%A`
+
+# Testing code - do not change it
+
+if [ "$BIRTHDATE" == "may 17, 1982" ] ; then
+    echo "BIRTHDATE is correct, it is $BIRTHDATE"
+else
+    echo "BIRTHDATE is incorrect - please retry"
+fi
+if [ $Presents == 10 ] ; then
+    echo "I have received $Presents presents"
+else
+    echo "Presents is incorrect - please retry"
+fi
+if [ "$BIRTHDAY" == "Monday" ] ; then
+    echo "I was born on a $BIRTHDAY"
+else
+    echo "BIRTHDAY is incorrect - please retry"
+fi
